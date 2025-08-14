b3e5e22de18e35e255c84c02176ec6cca18abe8b
Inara Oenning <inaraoenning@gmail.com>
2025-08-14 13:44:18 -0300

    Atividade Sistema de Vendas de Loja de Games

--- src/Sistema.java
+++ src/Sistema.java
@@ -1,11 +1,41 @@
 import com.sun.tools.javac.Main;
+import org.w3c.dom.ls.LSOutput;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 
 public class Sistema {
-    String tituloJogo=
+    String clienteNome;
+    String jogoNome;
+    String jogoGenero;
+    int quantidadeCompra;
+    int opcaoPagamento;
 
+    boolean pagamentoAvista;
+
+    //Pega data e hora atual,formata e armazena em String
+    LocalDateTime dataCompra = LocalDateTime.now();
+    DateTimeFormatter dataFormato = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+    String dataFormatada = dataCompra.format(dataFormato);
+
+
+//    static class Venda{
+//
+//        double precoUnitario;
+//        double valorTotal;  //Valor da compra
+//        double valorDesconto;
+//        double valorFinal; //Valor após o desconto
+//
+//        public double caclularTotal(){
+//
+//        }
+//    }
 
     static void main(String[] args) {
-        Main game = new Main();
+        Sistema s = new Sistema();
+        System.out.println(s.dataFormatada);
+
 
     }
 
